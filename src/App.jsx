b894ccc2a241313f331d18e4ee841551cc386d39
Hate Machine <53705490+HateMachine98@users.noreply.github.com>
2024-06-29 08:54:55 +0530

    Integrated Open AI API
    Open AI Functionality is not working need to fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,51 @@ import { Container, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TextInput from "./components/TextInput";
-
+import { useState } from "react";
+import KeywordModel from "./components/KeywordModel";
+// VITE_OPENAI_API_KEY='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'
+// VITE_OPENAI_API_URL='https://api.openai.com/v1/completions'
 const App = () => {
-  const extractKeyword = (text) => {
-    console.log("text:", text);
+  const [keywords, setKeywords] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const extractKeyword = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo-instruct",
+        prompt:
+          "Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n" +
+          text +
+          "",
+        temperature: 0.5,
+        max_tokens: 60,
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_OPENAI_API_URL,
+        options
+      );
+      const json = await response.json();
+      console.log(json.choices[0].text.trim());
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
   };
   return (
     <Box bg="blue.600" color="#fff" height="100vh" paddingTop={130}>
@@ -14,6 +55,12 @@ const App = () => {
         <TextInput extractKeyword={extractKeyword} />
         <Footer />
       </Container>
+      <KeywordModel
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
     </Box>
   );
 };
