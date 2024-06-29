b7df2e2a1cd5c98cc0e078087e3558170f63026b
Hate Machine <53705490+HateMachine98@users.noreply.github.com>
2024-06-29 08:54:55 +0530

    Integrated Open AI API
    Open AI Functionality is not working need to fix

---
+++ src/components/KeywordModel.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Button,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  CircularProgress,
+} from "@chakra-ui/react";
+
+const KeywordModel = ({ keywords, loading, isOpen, closeModal }) => {
+  return (
+    <div>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Keywords</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display="flex" justifyContent="center" alignItems="center">
+            {loading ? (
+              <CircularProgress isIndeterminate color="blue.300" />
+            ) : (
+              <Text>{keywords}</Text>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={closeModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+};
+
+export default KeywordModel;
